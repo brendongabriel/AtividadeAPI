d4a26758544539d61a240b63f7f692d01803e27d
Brendon <brendon3007gabriel@gmail.com>
2021-05-27 11:32:09 -0300

    Aprendendo sobre migrates

--- src/main/java/br/com/senai/api/controller/PessoaController.java
+++ src/main/java/br/com/senai/api/controller/PessoaController.java
@@ -1,14 +1,31 @@
 package br.com.senai.api.controller;
 
+import br.com.senai.domain.model.Pessoa;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class PessoaController {
 
 
     @GetMapping("/pessoas")
-    public String listar(){
-        return "Teste";
+    public List listar(){
+        Pessoa pessoa = new Pessoa();
+        pessoa.setId(1L);
+        pessoa.setNome("Brendin");
+        pessoa.setEmail("brendon_g_silva@estudante.sc.senai.br");
+        pessoa.setTelefone("(47) 90000-0000");
+
+        Pessoa pessoa2 = new Pessoa();
+        pessoa2.setId(2L);
+        pessoa2.setNome("Jhonis");
+        pessoa2.setEmail("Jhonis.gmail.com");
+        pessoa2.setTelefone("(47) 90000-0000");
+
+        return Arrays.asList(pessoa,pessoa2);
     }
 }
--- src/main/java/br/com/senai/domain/model/Pessoa.java
+++ src/main/java/br/com/senai/domain/model/Pessoa.java
@@ -1,41 +1,27 @@
 package br.com.senai.domain.model;
 
-public class Pessoa {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String nome;
+
     private String email;
+
+    @Column(name = "fone")
     private String telefone;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
 }
