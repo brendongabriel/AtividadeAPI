48bd22730b08f50666989a81739a4e79ea704704
Brendon <brendon3007gabriel@gmail.com>
2021-05-31 09:49:19 -0300

    CRUD

--- src/main/java/br/com/senai/domain/model/Pessoa.java
+++ src/main/java/br/com/senai/domain/model/Pessoa.java
@@ -1,26 +1,38 @@
 package br.com.senai.domain.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import static lombok.AccessLevel.PRIVATE;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
+@FieldDefaults(level = PRIVATE)
 public class Pessoa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+     long id;
 
-    private String nome;
+    @NotBlank
+    @Size(max = 60)
+    String nome;
 
-    private String email;
+    @NotBlank
+    @Email
+    @Size(min = 5)
+    String email;
 
-    private String telefone;
+    @NotBlank
+    @Size(min = 14)
+    String telefone;
 
 }
