9a9538801f9ab0b057b1f4d54c0d07a75733bc4f
Brendon <brendon3007gabriel@gmail.com>
2021-06-01 16:49:34 -0300

    Definições das exceptions

--- src/main/java/br/com/senai/domain/repository/PessoaRepository.java
+++ src/main/java/br/com/senai/domain/repository/PessoaRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
@@ -12,4 +13,6 @@ public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
     List<Pessoa> findByNome(String nome);
 
     List<Pessoa> findByNomeContaining(String nome);
+
+    Optional<Pessoa> findByEmail(String email);
 }
