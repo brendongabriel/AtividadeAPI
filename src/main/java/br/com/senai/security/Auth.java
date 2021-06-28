c5534522969cd6ede6b0ae6ae1cbc288dd47d711
Brendon <brendon3007gabriel@gmail.com>
2021-06-28 09:45:38 -0300

    Salvando o brayan

---
+++ src/main/java/br/com/senai/security/Auth.java
@@ -0,0 +1,8 @@
+package br.com.senai.security;
+
+import javax.persistence.criteria.CriteriaBuilder;
+
+public class Auth {
+    final static String secret = "4817fcfd50ebfd16e94db28a1b659584";
+    final static int expiresIn = 120000;
+}
